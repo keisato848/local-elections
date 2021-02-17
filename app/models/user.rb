4a59a13be1096e9a44fa9cd6a0da16f046cb816b
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-02-17 19:40:22 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates password:, format: {with: VALID_PASSWORD_REGEX}
+  validates :nickname,  presense: true, uniquness: true
+
+  private
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/.freeze
 end
