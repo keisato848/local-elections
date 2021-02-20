7e57a02aaf53cf1e90d55499c0c6096517a06cb7
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-02-20 21:21:02 +0900

    ヘッダーの表示をprefecturesテーブルに紐付け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :prefecture_selections
 
   private
-
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def prefecture_selections
+    @prefectures = Prefecture.all
   end
 end
