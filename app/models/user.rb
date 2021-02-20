8ecf358e7b1a59339f38e054c54bddc81189ad84
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-02-20 11:10:03 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates password:, format: {with: VALID_PASSWORD_REGEX}
-  validates :nickname,  presense: true, uniquness: true
-
-  private
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/.freeze
+         
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/.freeze
+         validates :password, format: {with: VALID_PASSWORD_REGEX}
+         validates :nickname,  presence: true, uniqueness: true         
 end
