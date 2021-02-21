6b896f3e0570a78370f88e6988ac298ac45ada77
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-02-21 11:16:50 +0900

    rubocopの実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :prefecture_selections
 
   private
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
