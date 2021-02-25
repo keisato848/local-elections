287aac4bcb681da4859bcb8ec5df64be1f366148
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-02-25 15:53:31 +0900

    コントローラーを修正

--- app/controllers/assemblymen_controller.rb
+++ app/controllers/assemblymen_controller.rb
@@ -1,22 +1,13 @@
 class AssemblymenController < ApplicationController
-  before_action :find_council, :find_assemblyman, :find_assemblymen
-  
+
   def index
+    @council = Council.find_by(id: params[:council_id])
+    @assemblyman = Assemblyman.find_by(council_id: params[:council_id])
+    @assemblymen = Assemblyman.where(council_id: params[:council_id])
   end
   
   def show
+    @assemblyman = Assemblyman.find_by(id: params[:id])
   end
-  
-  
-  private
-  def find_council
-    @council = Council.find_by(id: params[:council_id])
-  end
-  def find_assemblyman
-    @assemblyman = Assemblyman.find_by(council_id: params[:council_id])
-  end
-  
-  def find_assemblymen
-    @assemblymen = Assemblyman.where(council_id: params[:council_id])
-  end
+
 end
