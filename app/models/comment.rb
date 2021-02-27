ff092b61e60975aeb07708756992d359a3ad59b9
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-02-27 17:51:05 +0900

    バリデーションの設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :assemblyman
+
+  with_options presence: true do
+    validates :user_id    
+    validates :assemblyman_id    
+    validates :comment, length: {minimum: 3, maximum: 256}
+  end
 end
