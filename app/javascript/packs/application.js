5363ab2575d7ef282db3a35bb34f929d85536a64
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-02 19:06:21 +0900

    ヘッダー・フッターの再修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 require('header.js')
+require('index.js')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
