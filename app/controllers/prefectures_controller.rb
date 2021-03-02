1c8b359de256ba60df266417d0293e83718febfb
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-03 00:14:31 +0900

    議会一覧ページの編集

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,6 +1,7 @@
 class PrefecturesController < ApplicationController
   def show
     @prefecture = Prefecture.find(params[:id])
+    @governors = Council.where("name LIKE ? AND prefecture_id LIKE ?", "%首長%", "#{params[:id]}")
     @councils = Council.where(prefecture_id: params[:id])
   end
 end
