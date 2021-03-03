c9931f4650449f0e02f46052fb4c93af8e73b9d3
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-03 16:25:05 +0900

    静的コードの解析
    rubocopの実行

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,7 +1,7 @@
 class PrefecturesController < ApplicationController
   def show
     @prefecture = Prefecture.find(params[:id])
-    @governors = Council.where("name LIKE ? AND prefecture_id LIKE ?", "%首長%", "#{params[:id]}")
+    @governors = Council.where('name LIKE ? AND prefecture_id LIKE ?', '%首長%', params[:id].to_s)
     @councils = Council.where(prefecture_id: params[:id])
   end
 end
