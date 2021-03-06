3dbff98026ce0a57c9050d7f10eb51e42716e4e5
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-06 11:32:07 +0900

    各ページをレスポンシブ対応に変更

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,7 +1,6 @@
 class PrefecturesController < ApplicationController
   def show
     @prefecture = Prefecture.find(params[:id])
-    @governors = Council.where('name LIKE ? AND prefecture_id LIKE ?', '%首長%', params[:id].to_s)
     @councils = Council.where(prefecture_id: params[:id])
   end
 end
