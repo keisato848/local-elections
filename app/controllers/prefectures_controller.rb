353508feb6b14c9be491e414667da865fe63581e
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-10 21:03:55 +0900

    コントローラーの設定

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,6 +1,8 @@
 class PrefecturesController < ApplicationController
+  
   def show
     @prefecture = Prefecture.find(params[:id])
     @councils = Council.where(prefecture_id: params[:id])
   end
+  
 end
