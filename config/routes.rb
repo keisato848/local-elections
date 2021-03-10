fb83fa15d125ae97206ac7e7b5a4364e14e6a7ed
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-10 21:03:15 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'search/index'
   devise_for :users
   root to: 'prefectures#index'
   resources :prefectures, only: [:index, :show] do
-    resources :councils, only: :show do
+    resources :councils, only: [:show, :search] do
+      member do
+        get 'search'
+        post 'search_result' 
+      end
       resources :assemblymen, only: [:show] do
         resources :comments, only: [:new, :create]
       end
