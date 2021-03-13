3e07585b34a5fcd710338cd33d563cb79eea7035
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-13 10:32:31 +0900

    rubocopの実行

--- app/controllers/councils_controller.rb
+++ app/controllers/councils_controller.rb
@@ -1,16 +1,16 @@
 class CouncilsController < ApplicationController
   before_action :search_council
-  
+
   def show
     @assemblyman = Assemblyman.find_by(council_id: params[:id])
     @assemblymen = Assemblyman.where(council_id: params[:id])
   end
-  
+
   def search
     select_content
     search_params
   end
-  
+
   def search_result
     if params[:faction].empty? && params[:sex].empty? && params[:job].empty?
       incorrect_search
@@ -18,8 +18,9 @@ class CouncilsController < ApplicationController
       search_target
     end
   end
-    
+
   private
+
   def search_council
     @council = Council.find_by(id: params[:id])
   end
@@ -43,7 +44,7 @@ class CouncilsController < ApplicationController
     search_params
     render :search
   end
-  
+
   def search_target
     # フォームに入力された値を変数に格納
     faction = params[:faction]
@@ -52,7 +53,7 @@ class CouncilsController < ApplicationController
     # すべての値が入力された場合
     if faction.present? && sex.present? && job.present?
       @all_match = Assemblyman.where(faction: faction, sex: sex, job: job)
-      if @all_match.empty? 
+      if @all_match.empty?
         @faction_match = Assemblyman.where(faction: faction)
         @sex_match = Assemblyman.where(sex: sex)
         @job_match = Assemblyman.where(job: job)
@@ -60,19 +61,19 @@ class CouncilsController < ApplicationController
     # ２つの値が入力された場合
     elsif faction.empty? && (sex.present? && job.present?)
       @all_match = Assemblyman.where(sex: sex, job: job)
-      if @all_match.empty? 
+      if @all_match.empty?
         @sex_match = Assemblyman.where(sex: sex)
         @job_match = Assemblyman.where(job: job)
       end
     elsif sex.empty? && (faction.present? && job.present?)
       @all_match = Assemblyman.where(faction: faction, job: job)
-      if @all_match.empty? 
+      if @all_match.empty?
         @faction_match = Assemblyman.where(faction: faction)
         @job_match = Assemblyman.where(job: job)
       end
     elsif job.empty? && (faction.present? && sex.present?)
       @all_match = Assemblyman.where(faction: faction, sex: sex)
-      if @all_match.empty? 
+      if @all_match.empty?
         @faction_match = Assemblyman.where(faction: faction)
         @sex_match = Assemblyman.where(sex: sex)
       end
--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,8 +1,6 @@
 class PrefecturesController < ApplicationController
-  
   def show
     @prefecture = Prefecture.find(params[:id])
     @councils = Council.where(prefecture_id: params[:id])
   end
-  
 end
