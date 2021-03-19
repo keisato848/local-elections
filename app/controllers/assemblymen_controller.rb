a97e38b58ec4b815ed155f4c24b0acc4c17ca156
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-19 17:21:52 +0900

    コントローラーの設定

--- app/controllers/assemblymen_controller.rb
+++ app/controllers/assemblymen_controller.rb
@@ -1,12 +1,29 @@
 class AssemblymenController < ApplicationController
-  before_action :search_comment
+  before_action :search_comment, only: :show
   def show
-    @assemblyman = Assemblyman.find_by(id: params[:id])
+    find_assemblyman
+  end
+  def edit
+    find_assemblyman
+  end
+  def update
+    find_assemblyman
+    if @assemblyman.update(assemblyman_params)
+      redirect_to prefecture_council_assemblyman_path(params[:id])
+    else
+      find_assemblyman
+      render :update
+    end
   end
 
   private
-
+  def find_assemblyman
+    @assemblyman = Assemblyman.find_by(id: params[:id])
+  end
   def search_comment
     @comments = Comment.where(assemblyman_id: params[:id])
+  end
+  def assemblyman_params
+    params.require(:assemblyman).permit(:name, :sex, :birth_of_date, :position, :faction, :number_of_wins, :job, :img_url, :twitter_url)
   end
 end
