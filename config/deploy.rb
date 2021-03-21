faf0fd41edf61651f0b22c70749b7c0eb8d85ba9
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-21 23:19:57 +0900

    自動デプロイの設定

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+lock '3.16.0'
+set :application, 'local-elections'
+set :repo_url,  'https://github.com/keisato848/local-elections.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/rabbitmoka.pem'] 
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
