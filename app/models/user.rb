745f967ac5f4bcf539d118dc052696b2c26870fd
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-24 15:26:38 +0900

    deviseを継承したUserモデルを作成し、deviseコントローラーを再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,24 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2, :twitter]
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true, uniqueness: true
 
   has_many :comments, dependent: :destroy
+  has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   root to: 'prefectures#index'
   resources :prefectures, only: [:index, :show] do
     resources :councils, only: [:show, :search] do
