7515a9e6678c20c13f4ef8c7d61a55ac7522ba95
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-26 20:06:56 +0900

    マイグレーションファイルの編集

---
+++ db/migrate/20210326105931_create_manifests.rb
@@ -0,0 +1,9 @@
+class CreateManifests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :manifests do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.timestamps
+    end
+  end
+end
