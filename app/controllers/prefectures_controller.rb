f58d939b8084e6ae88ed01823f0a3ad29cba1c19
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-03-27 18:08:24 +0900

    マニュフェスト一覧をトップページに表示

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,4 +1,7 @@
 class PrefecturesController < ApplicationController
+  def index
+    @manifests = Manifest.last(5)
+  end
   def show
     @prefecture = Prefecture.find(params[:id])
     @councils = Council.where(prefecture_id: params[:id])
