590b71f42fd9c32df333bf293f0b4898eff1aacf
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-04-08 14:13:11 +0900

    キーペア変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/rabbitmoka.pem'] 
+                                  keys: ['~/.ssh/local-elections.pem'] 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
