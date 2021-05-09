16ce90f2ce293172ce0d647b5881625d7be3eb81
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-05-09 16:30:00 +0900

    トップページの設定

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,5 +1,6 @@
 class PrefecturesController < ApplicationController
   def index
     @manifests = Manifest.last(5).sort { |a, b| b <=> a }
+    @questions = Question.last(5).sort { |a, b| b <=> a }
   end
 end
