41f3a609be2cb0e26e7f9f498282946ba643bb08
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-05-11 22:31:12 +0900

    タグ一覧ページのフロント修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,7 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find(params[:id])
-    @manifests = Manifest.where(id: @tag.manifest_ids)
+    @manifests = Manifest.all.includes(:tags)
+    @questions = Question.all.includes(:tags)
   end
 end
