5f5e95251223f7245f1887a2044b7aaec2d2882f
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-05-18 23:23:43 +0900

    バリデーションの修正

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -2,5 +2,5 @@ class Manifest < ApplicationRecord
   has_many :manifest_tag_relations
   has_many :tags, through: :manifest_tag_relations
   belongs_to :user
-  has_many :manifest_comments
+  has_many :manifest_comments, dependent: :destroy
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :question_tag_relations
   has_many :tags, through: :question_tag_relations
-  has_many :question_comments
+  has_many :question_comments, dependent: :destroy
 end
