a83fdecb7cea2295b3a39e08b8dc31e6f3e08fc9
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-06-04 20:04:13 +0900

    fix sign_in_support

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -3,7 +3,7 @@ module SignInSupport
     visit new_user_session_path
     fill_in 'email', with: user.email
     fill_in 'password', with: user.password
-    click_on 'ログイン'
+    find('.register-blue-btn').click
     expect(current_path).to eq(root_path)
   end
 end
